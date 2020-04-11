69186a4ab115212d48c852ff354008608058171f
brandondev <brandondev@brandondevs-Mac-mini.local>
2020-04-11 14:15:08 -0400

    first commit

---
+++ src/components/Button.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="center">
+    <div>
+      <h1>Testing</h1>
+      <button @click="increaseCount">Click Me</button>
+      <p>{{ count }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        count: 0
+      }
+    },
+    methods: {
+      increaseCount() {
+        this.count++
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .center {
+    text-align: center;
+  }
+</style>
